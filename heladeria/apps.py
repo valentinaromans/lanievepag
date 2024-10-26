b505413f9e255d98aff2b9395e018adea9cbdd95
valentinaromans <valentina.roman03@inacapmail.cl>
2024-10-26 19:25:25 -0300

    app inicio eliminada y creación de nuevas apps

---
+++ heladeria/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HeladeriaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'heladeria'
--- productos/apps.py
+++ productos/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class InicioConfig(AppConfig):
+class ProductosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inicio'
+    name = 'productos'
